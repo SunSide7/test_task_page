b4a7783a3ced26636f0312fa4ef50509922b1e8d
Ascended <Ascended@mail.com>
2020-07-26 00:12:00 +0200

    Refactoring done

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ function renderElementFromJSON(
     iterateKey,
     tablePartType
 ) {
-    const tableNodeByType = () => {
-        switch (tablePartType) {
+    const tableNodeByType = type => {
+        switch (type) {
             case 'head':
                 iterateKey = iterateKey.replace('_', ' ');
                 iterateKey = iterateKey.charAt(0).toUpperCase() + iterateKey.slice(1);
@@ -27,25 +27,28 @@ function renderElementFromJSON(
     
     tablePartNode.lastElementChild.insertAdjacentHTML(
       "afterbegin",
-      tableNodeByType()
+      tableNodeByType(tablePartType)
     );
     
 }
 
 fetch("https://api.npoint.io/1a33ecf254970beccc52")
-  .then((response) => response.json())
-  .then((json) => {
+  .then(response => response.json())
+  .then(json => {
       
     // Table Head
     tableHead.insertAdjacentHTML("beforeend", "<tr></tr>");
 
-    Object.keys(json[0]).forEach((key) => renderElementFromJSON(tableHead, key, 'head'));
+    Object.keys(json[0])
+        .forEach(key => renderElementFromJSON(tableHead, key, 'head'));
 
+    
     // Table Body
-    json.forEach((item) => {
+    json.forEach(item => {
       tableBody.insertAdjacentHTML("beforeend", "<tr></tr>");
 
-      Object.values(item).forEach((value) => renderElementFromJSON(tableBody, value, 'body'));
+      Object.values(item)
+        .forEach(value => renderElementFromJSON(tableBody, value, 'body'));
     });
 
   });
